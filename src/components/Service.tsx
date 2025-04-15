d158a8b6fdfca6a94adf23001eb51620e434a601
Jesus Pichon <jesuspichonramirez@gmail.com>
2025-04-15 10:52:25 -0600

    modificando el componente service para unificarlo con el diseño principal

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -22,16 +22,16 @@ const Service: React.FC<ServiceCardProps> = ({ title, src, text }) => {
                 className={`rounded-full w-50 h-50 object-cover mb-4 transform transition-transform duration-300 ${visible ? '-translate-y-4 scale-105' : ''}`}
             />
 
+            <p className={`font-semibold text-white text-2xl mb-1 transform transition-transform duration-300 ${visible ? '-translate-y-4 scale-105' : ''}`}>{title}</p>
 
             <div
                 className={`text-center transition-opacity duration-300 ${visible ? 'opacity-100' : 'opacity-0'}`}
             >
-                <p className="mb-4 font-semibold">{title}</p>
-                <p className="w-64 mb-6">{text}</p>
+                <p className="text-white w-64 mb-6">{text}</p>
                 <input
                     type="button"
-                    value="más infórmacion"
-                    className="border-2 border-solid"
+                    value="más información"
+                    className="border border-amber-600 font-semibold text-xl text-white p-2 hover:text-amber-600"
                 />
             </div>
         </div>
